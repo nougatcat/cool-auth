5c2ca94af5418e68905b03ca359242d80b8346f2
nougatcat <105752568+nougatcat@users.noreply.github.com>
2025-10-17 02:57:02 +0300

    Search layout 80% done, 20px font in globals.css

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,36 @@
+// import { cn } from '@/lib/utils'
+import css from './page.module.css'
 
 export default function Search() {
     return (
-        <div>Здесь должен выводиться список документов</div>
+        <div className={css.container}>
+            <div className="m-5">
+                <div className="flex justify-between mb-5">
+                    <div className={css.button_create}>Создать</div>
+                    <div><input className={css.button_search} type="text" placeholder='Введите поисковой запрос' /></div>
+                    <div className={css.button_profile}>Профиль</div>
+                </div>
+                <div className='shadow-[0px_0px_4px_0px_rgba(0,0,0,0.25)]'>
+                    <div className={css.row_head}>
+                        <div className='w-[60%]'>Название</div>
+                        {/* <div className='w-[60%] p-[10px] box-border'>Название</div> */}
+                         {/* border-box; /* чтобы padding не увеличивал ширину. ВНЕЗАПНО паддинг теперь работает у родителя и смещения дочернего блока нет. ККААААККК */}
+                        <div className='w-[20%]'>Владелец</div>
+                        <div className='w-[20%]'>Права доступа</div>
+                    </div>
+                    <div className={css.row}>
+                        <div className='w-[60%]'>документ очень важный</div>
+                        <div className='w-[20%]'>Олег</div>
+                        <div className='w-[20%]'>Чтение</div>
+                    </div>
+                    <div className={css.row + ' ' + css.row_green}>
+                        <div className='w-[60%]'>документ очень важный ывывы ы вывывыфвфыв фыв фв фыафы афыа фы афы фы фы вы вфв фыфы вфы вфы</div>
+                        <div className='w-[20%]'>Олег олег олег олег олег</div>
+                        <div className='w-[20%]'>Чтение/Запись</div>
+                    </div>
+                    <div className={css.row_foot}></div>
+                </div>
+            </div>
+        </div>
     )
 }
