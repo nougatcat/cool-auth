c0845a16b583a4fb61378806cc1bd5ec9bcf0fe9
nougatcat <105752568+nougatcat@users.noreply.github.com>
2025-10-22 17:38:35 +0300

    prisma schema complete, prisma seed complete

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,10 +23,46 @@ async function up() {
             },
         ]
     });
+    await prisma.document.createMany({
+        data: [
+            {
+                authorId: 2,
+                content: 'Блаблаблаблаблаблаблаблаблаблаблабла ьалаб лаба ла лааб ал аб а',
+                title: 'Мой документ RW',
+                adminPerms: 'RW',
+                userPerms: 'READ',
+            },
+            {
+                authorId: 2,
+                content: 'aaa aa aa aa Блаблаблаблаблаблаблаблаблаблаблабла ьалаб лаба ла лааб ал аб а',
+                title: 'Мой документ R',
+                adminPerms: 'READ',
+                userPerms: 'READ',
+            },
+        ]
+    });
+    await prisma.docHistory.createMany({
+        data: [
+            {
+                documentId: 1,
+                editorId: 2,
+            },
+            {
+                documentId: 1,
+                editorId: 1,
+            },
+            {
+                documentId: 2,
+                editorId: 2,
+            },
+        ]
+    });
 }
 
 async function down() {
     await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE "Document" RESTART IDENTITY CASCADE`
+    await prisma.$executeRaw`TRUNCATE TABLE "DocHistory" RESTART IDENTITY CASCADE`
     // TRUNCATE TABLE Удаляет все строки из таблицы, но структура таблицы и ее столбцы, ограничения, индексы и т. д. остаются. 
     // RESTART IDENTITY значит очищаем сам id, т.е. другой юзер сможет получить этот id
     // удалить все данные пользователя и удалить данные в других таблицах связанные с ним (CASCADE) 
@@ -44,6 +80,7 @@ async function main() {
 main()
     .then(async () => {
         await prisma.$disconnect()
+        process.exit(0); //! без этой строки сид может зависнуть
     })
     .catch(async (e) => {
         console.error(e)
