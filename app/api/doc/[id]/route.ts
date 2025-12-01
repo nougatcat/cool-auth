fab984f07543564325d84c447ec5e596b483e9a8
nougatcat <105752568+nougatcat@users.noreply.github.com>
2025-12-01 18:05:28 +0300

    Api выдающее один документ по параметру из url

--- app/api/doc/[id]/route.ts
+++ app/api/doc/[id]/route.ts
@@ -1,1 +1,75 @@
 // Тут должен быть DELETE запрос и PATCH запрос, если я не пропишу его в сервер экшен
+
+// тут должен быть GET запрос на вывод всех доступных документов
+import { prisma } from "@/prisma/prisma-client"
+import { getUserSession } from "@/lib/get-user-session"
+import { NextResponse } from "next/server"
+
+export const dynamic = 'force-dynamic' //! есть баг с асинхронным контекстом на уровне next-js, поэтому билд может не работать в этом месте
+
+export async function GET(request: Request, {params} : {params: Promise <{id: string}>}) { //! Начиная с версии next js 15 нужно оборачивать тип в промис и прописывать request: Request
+    try {
+        const {id} = await params
+        const id_number = Number(id)
+        const user = await getUserSession()
+        if (!user) {
+            return NextResponse.json({ message: 'Вы не авторизованы' }, { status: 401 })
+        }
+        let data = null
+        if (user.role === 'ADMIN') {
+            data = await prisma.document.findFirst({
+                where: {
+                    id: id_number,
+                    OR: [
+                        {
+                            adminPerms: {
+                                in: ["READ", "RW"]
+                            }
+                        },
+                        {
+                            adminPerms: "NONE",
+                            authorId: Number(user.id)
+                        }
+                    ]
+                },
+                include: {
+                    author: {
+                        select: {
+                            name: true
+                        }
+                    }
+                }
+            })
+        } else if (user.role = 'USER') {
+            data = await prisma.document.findFirst({
+                where: {
+                    id: id_number,
+                    OR: [
+                        {
+                            userPerms: {
+                                in: ["READ", "RW"]
+                            }
+                        },
+                        {
+                            userPerms: "NONE",
+                            authorId: Number(user.id)
+                        }
+                    ]
+                },
+                include: {
+                    author: {
+                        select: {
+                            name: true
+                        }
+                    }
+                }
+            })
+        }
+        return NextResponse.json(data)
+    } catch (e) {
+        console.log(e)
+        return NextResponse.json({ message: '[USER_GET] Server error' }, { status: 500 })
+    }
+}
+
+// TODO title и content документа лучше шифровать, чтобы при сливе бд нельзя было слить документы. Делать это нужно в post и patch запросах. На данном этапе разработки необязательно
